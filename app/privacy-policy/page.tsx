9442dd4e56c779fa20abd0542e301523e922caf6
kennedyngigi4 <kennedyngigi4@gmail.com>
2025-11-24 18:43:28 +0300

    Privacy policy fix

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PrivacyPolicy = () => {
   return (
-    <section className="flex flex-col min-h-screen">
+    <section className="flex flex-col min-h-screen mx-20 my-10">
         <p className="text-slate-500 text-sm">Last Update: 24th Nov, 2025</p>
         <h1 className="text-xl font-bold pt-4">Privacy Policy</h1>
     </section>
