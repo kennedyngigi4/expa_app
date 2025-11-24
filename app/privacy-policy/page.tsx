b3108581a83a2af577bcab7af22d3541a8a7b362
kennedyngigi4 <kennedyngigi4@gmail.com>
2025-11-24 17:49:55 +0300

    Added privacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PrivacyPolicy = () => {
+  return (
+    <section className="flex flex-col min-h-screen">
+        <p className="text-slate-500 text-sm">Last Update: 24th Nov, 2025</p>
+        <h1 className="text-xl font-bold pt-4">Privacy Policy</h1>
+    </section>
+  )
+}
+
+export default PrivacyPolicy
