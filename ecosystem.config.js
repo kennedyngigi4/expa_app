c3dfcc179c4c64d647fc21c2576e66c42839ae41
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-01-02 10:05:29 +0300

    Fix hard memory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
             exec_mode: "fork",
             instances: 1,
             max_memory_restart: "350M",
+            node_args: "--max-old-space-size=384",
             env: {
                 NODE_ENV: "production",
             },
